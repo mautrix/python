4158dba9a69ae6b02453352d87b1c43cc0d03a51
Tulir Asokan <tulir@maunium.net>
2018-04-26 20:50:10 +0300

    Bump version to 0.1.4

--- mautrix_appservice/__init__.py
+++ mautrix_appservice/__init__.py
@@ -1,5 +1,5 @@
 from .appservice import AppService
 from .errors import MatrixError, MatrixRequestError, IntentError
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "Tulir Asokan <tulir@maunium.net>"
