122ac0d2cf7cb8701c973a4ddf129c3b50030c70
Tulir Asokan <tulir@maunium.net>
2018-05-06 20:30:24 +0300

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mautrix-appservice",
-    version="0.1.4",
+    version="0.1.5",
     url="https://github.com/tulir/mautrix-appservice-python",
 
     author="Tulir Asokan",
