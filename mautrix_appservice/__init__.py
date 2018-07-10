ce04e3a542f70a7cb4c82557a13b11e46894ae05
Tulir Asokan <tulir@maunium.net>
2018-07-10 14:33:02 +0300

    Export IntentAPI

--- mautrix_appservice/__init__.py
+++ mautrix_appservice/__init__.py
@@ -1,6 +1,7 @@
 from .appservice import AppService
 from .errors import MatrixError, MatrixRequestError, IntentError
 from .state_store import StateStore
+from .intent_api import IntentAPI
 
 __version__ = "0.3.0+dev"
 __author__ = "Tulir Asokan <tulir@maunium.net>"
