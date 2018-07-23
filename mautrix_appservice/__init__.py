f905169339875ab5fc6570af84b9b9147c774396
Tulir Asokan <tulir@maunium.net>
2018-07-23 11:50:40 -0400

    Bump version to 0.3.2

--- mautrix_appservice/__init__.py
+++ mautrix_appservice/__init__.py
@@ -3,5 +3,5 @@ from .errors import MatrixError, MatrixRequestError, IntentError
 from .state_store import StateStore
 from .intent_api import IntentAPI
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __author__ = "Tulir Asokan <tulir@maunium.net>"
