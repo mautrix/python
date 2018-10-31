acc918f1d974fce8e22183d4c619228f0d2d7e5a
Tulir Asokan <tulir@maunium.net>
2018-11-01 01:49:27 +0200

    Add client.whoami()

---
+++ mautrix/client/api/authentication.py
@@ -0,0 +1,33 @@
+from ...errors import MatrixResponseError
+from ...api import Method, Path
+from .types import UserID
+from .base import BaseClientAPI
+
+
+class ClientAuthenticationMethods(BaseClientAPI):
+    """
+    Methods in section 5 Authentication of the spec. These methods are used for setting and getting user
+    metadata and searching for users.
+
+    See also: `API reference <https://matrix.org/docs/spec/client_server/r0.4.0.html#client-authentication>`__
+    """
+
+    # TODO other sections
+
+    # region 5.7 Current account information
+    # API reference: https://matrix.org/docs/spec/client_server/r0.4.0.html#current-account-information
+
+    async def whoami(self) -> UserID:
+        """
+        Get information about the current user.
+
+        Returns:
+            The user ID of the current user.
+        """
+        resp = await self.api.request(Method.GET, Path.account.whoami)
+        try:
+            return resp["user_id"]
+        except KeyError:
+            raise MatrixResponseError("`user_id` not in response.")
+
+    # endregion
--- mautrix/client/api/client.py
+++ mautrix/client/api/client.py
@@ -1,3 +1,4 @@
+from .authentication import ClientAuthenticationMethods
 from .filtering import FilteringMethods
 from .events import EventMethods
 from .rooms import RoomMethods
@@ -5,7 +6,8 @@ from .user_data import UserDataMethods
 from .modules import ModuleMethods
 
 
-class ClientAPI(FilteringMethods, EventMethods, RoomMethods, UserDataMethods, ModuleMethods):
+class ClientAPI(ClientAuthenticationMethods, FilteringMethods, EventMethods, RoomMethods,
+                UserDataMethods, ModuleMethods):
     """
     ClientAPI is a medium-level wrapper around the HTTPAPI that provides many easy-to-use
     functions for accessing the client-server API.
