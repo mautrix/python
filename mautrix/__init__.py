21991358646c7ebcdf5c80242d96610cf3416a2f
Tulir Asokan <tulir@maunium.net>
2019-07-21 20:42:55 +0300

    Remove importing everything in __init__.py

--- mautrix/__init__.py
+++ mautrix/__init__.py
@@ -1,7 +1,2 @@
-# Copyright (c) 2019 Tulir Asokan
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from .__meta__ import __version__
-from . import api, appservice, bridge, client, errors, util
+__version__ = "0.4.0.dev48"
+__author__ = "Tulir Asokan <tulir@maunium.net>"
