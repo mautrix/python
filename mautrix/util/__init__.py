f48b65cca44d002272bb4cbbb533cd79e22ffad2
Tulir Asokan <tulir@maunium.net>
2019-08-11 02:34:44 +0300

    Add simple unix socket manhole to utils

--- mautrix/util/__init__.py
+++ mautrix/util/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["formatter", "color_log", "config", "signed_token", "simple_template"]
+__all__ = ["formatter", "color_log", "config", "signed_token", "simple_template", "manhole"]
---
+++ mautrix/util/manhole.py
@@ -0,0 +1,294 @@
+# Copyright (c) 2018 Tulir Asokan
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Based on https://github.com/nhoad/aiomanhole Copyright (c) 2014, Nathan Hoad
+from typing import Any, Tuple, Optional, Dict, Union, List, Callable, Type
+from socket import SOL_SOCKET, SO_PEERCRED
+from abc import ABC, abstractmethod
+from io import BytesIO, StringIO
+from types import CodeType
+import contextlib
+import functools
+import traceback
+import logging
+import asyncio
+import struct
+import codeop
+import pwd
+import ast
+
+log = logging.getLogger("mau.manhole")
+
+
+class StatefulCommandCompiler(codeop.CommandCompiler):
+    """A command compiler that buffers input until a full command is available."""
+
+    buf: BytesIO
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.compiler = functools.partial(compile, optimize=1,
+                                          flags=ast.PyCF_ONLY_AST | codeop.PyCF_DONT_IMPLY_DEDENT)
+        self.buf = BytesIO()
+
+    def is_partial_command(self) -> bool:
+        return bool(self.buf.getvalue())
+
+    def __call__(self, source: bytes, **kwargs: Any) -> ast.AST:
+        buf = self.buf
+        if self.is_partial_command():
+            buf.write(b'\n')
+        buf.write(source)
+
+        code = self.buf.getvalue().decode('utf8')
+
+        codeobj = super().__call__(code, **kwargs)
+
+        if codeobj:
+            self.reset()
+        return codeobj
+
+    def reset(self) -> None:
+        self.buf.seek(0)
+        self.buf.truncate(0)
+
+
+class Interpreter(ABC):
+    @abstractmethod
+    def __init__(self, namespace: Dict[str, Any], banner: Union[bytes, str],
+                 loop: asyncio.AbstractEventLoop) -> None:
+        pass
+
+    @abstractmethod
+    def close(self) -> None:
+        pass
+
+    @abstractmethod
+    async def __call__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+        pass
+
+
+class AsyncInterpreter(Interpreter):
+    """An interactive asynchronous interpreter."""
+
+    reader: asyncio.StreamReader
+    writer: asyncio.StreamWriter
+    namespace: Dict[str, Any]
+    banner: bytes
+    compiler: StatefulCommandCompiler
+    loop: asyncio.AbstractEventLoop
+    running: bool
+    wrapper: str = """
+async def _eval_async_expr():
+    try:
+        pass
+    finally:
+        globals().update(locals())
+"""
+
+    def __init__(self, namespace: Dict[str, Any], banner: Union[bytes, str],
+                 loop: asyncio.AbstractEventLoop) -> None:
+        super().__init__(namespace, banner, loop)
+        self.namespace = namespace
+        self.banner = banner if isinstance(banner, bytes) else str(banner).encode("utf-8")
+        self.compiler = StatefulCommandCompiler()
+        self.loop = loop
+
+    # From https://gist.github.com/nitros12/2c3c265813121492655bc95aa54da6b9
+    def _insert_returns(self, body: List[ast.AST]) -> None:
+        if isinstance(body[-1], ast.Expr):
+            body[-1] = ast.Return(body[-1].value)
+            ast.fix_missing_locations(body[-1])
+        elif isinstance(body[-1], ast.If):
+            self._insert_returns(body[-1].body)
+            self._insert_returns(body[-1].orelse)
+        elif isinstance(body[-1], (ast.With, ast.AsyncWith)):
+            self._insert_returns(body[-1].body)
+
+    def attempt_compile(self, line: bytes) -> Optional[CodeType]:
+        ast_obj = self.compiler(line)
+        if not ast_obj:
+            return None
+
+        self._insert_returns(ast_obj.body)
+        wrapper = ast.parse(self.wrapper, "<ast>", "single")
+        method_stmt = wrapper.body[0]
+        try_stmt = method_stmt.body[0]
+        try_stmt.body = ast_obj.body
+        return compile(wrapper, "<ast>", "single")
+
+    async def send_exception(self) -> None:
+        """When an exception has occurred, write the traceback to the user."""
+        self.compiler.reset()
+
+        exc = traceback.format_exc()
+        self.writer.write(exc.encode('utf8'))
+
+        await self.writer.drain()
+
+    async def attempt_exec(self, codeobj: CodeType) -> Tuple[Any, str]:
+        with contextlib.redirect_stdout(StringIO()) as buf:
+            exec(codeobj, self.namespace)
+            value = await eval("_eval_async_expr()", self.namespace)
+
+        return value, buf.getvalue()
+
+    async def handle_one_command(self) -> None:
+        """Process a single command. May have many lines."""
+
+        while True:
+            await self.write_prompt()
+            codeobj = await self.read_command()
+
+            if codeobj is not None:
+                await self.run_command(codeobj)
+
+    async def run_command(self, codeobj: CodeType) -> None:
+        """Execute a compiled code object, and write the output back to the client."""
+        try:
+            value, stdout = await self.attempt_exec(codeobj)
+        except Exception:
+            await self.send_exception()
+            return
+        else:
+            await self.send_output(value, stdout)
+
+    async def write_prompt(self) -> None:
+        writer = self.writer
+
+        if self.compiler.is_partial_command():
+            writer.write(b"... ")
+        else:
+            writer.write(b">>> ")
+
+        await writer.drain()
+
+    async def read_command(self) -> Optional[CodeType]:
+        """Read a command from the user line by line.
+
+        Returns a code object suitable for execution.
+        """
+
+        reader = self.reader
+
+        line = await reader.readline()
+        if line == b'':  # lost connection
+            raise ConnectionResetError()
+
+        try:
+            # skip the newline to make CommandCompiler work as advertised
+            codeobj = self.attempt_compile(line.rstrip(b'\n'))
+        except SyntaxError:
+            await self.send_exception()
+            return None
+
+        return codeobj
+
+    async def send_output(self, value: str, stdout: str) -> None:
+        """Write the output or value of the expression back to user.
+
+        >>> 5
+        5
+        >>> print('cash rules everything around me')
+        cash rules everything around me
+        """
+
+        writer = self.writer
+
+        if value is not None:
+            writer.write('{!r}\n'.format(value).encode('utf8'))
+
+        if stdout:
+            writer.write(stdout.encode('utf8'))
+
+        await writer.drain()
+
+    def close(self) -> None:
+        if self.running:
+            self.writer.close()
+            self.running = False
+
+    async def __call__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+        """Main entry point for an interpreter session with a single client."""
+        self.reader = reader
+        self.writer = writer
+        self.running = True
+
+        if self.banner:
+            writer.write(self.banner)
+            await writer.drain()
+
+        while self.running:
+            try:
+                await self.handle_one_command()
+            except ConnectionResetError:
+                writer.close()
+                self.running = False
+                break
+            except Exception:
+                traceback.print_exc()
+
+
+class InterpreterFactory:
+    namespace: Dict[str, Any]
+    banner: bytes
+    loop: asyncio.AbstractEventLoop
+    interpreter_class: Type[Interpreter]
+    clients: List[Interpreter]
+
+    def __init__(self, namespace: Dict[str, Any], banner: Union[bytes, str],
+                 interpreter_class: Type[Interpreter], loop: asyncio.AbstractEventLoop) -> None:
+        self.namespace = namespace
+        self.banner = banner
+        self.loop = loop
+        self.interpreter_class = interpreter_class
+        self.clients = []
+
+    async def __call__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter
+                       ) -> None:
+        namespace = {**self.namespace}
+        interpreter = self.interpreter_class(namespace=namespace, banner=self.banner,
+                                             loop=self.loop)
+        namespace["exit"] = interpreter.close
+        self.clients.append(interpreter)
+        conn_id = len(self.clients)
+
+        sock = writer.transport.get_extra_info("socket")
+        creds = sock.getsockopt(SOL_SOCKET, SO_PEERCRED, struct.calcsize('3i'))
+        pid, uid, gid = struct.unpack('3i', creds)
+        user_info = pwd.getpwuid(uid)
+        username = user_info.pw_name if user_info and user_info.pw_name else uid
+        log.info(f"Manhole connection OPENED: {conn_id} from PID {pid} by {username}")
+        await asyncio.ensure_future(interpreter(reader, writer))
+        log.info(f"Manhole connection CLOSED: {conn_id} from PID {pid} by {username}")
+        self.clients.remove(interpreter)
+
+
+async def start_manhole(path: str, banner: str = "", namespace: Optional[Dict[str, Any]] = None,
+                        loop: asyncio.AbstractEventLoop = None
+                        ) -> Tuple[asyncio.AbstractServer, Callable[[], None]]:
+    """
+    Starts a manhole server on a given UNIX address.
+
+    Args:
+        path: The path to create the UNIX socket at.
+        banner: The banner to show when clients connect.
+        namespace: The globals to provide to connected clients.
+        loop: The asyncio event loop to use.
+    """
+    loop = loop or asyncio.get_event_loop()
+    namespace = namespace or {}
+    factory = InterpreterFactory(namespace=namespace, banner=banner,
+                                 interpreter_class=AsyncInterpreter, loop=loop)
+    server = await asyncio.start_unix_server(factory, path=path, loop=loop)
+
+    def stop():
+        for client in factory.clients:
+            client.close()
+        server.close()
+
+    namespace["stop"] = stop
+    return server, stop
