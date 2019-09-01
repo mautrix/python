8409126efceeacdef8d4ec7cbb0c5f26d1f4ad9a
Tulir Asokan <tulir@maunium.net>
2019-09-01 15:02:08 +0300

    Make new pypi dev build

--- mautrix/__init__.py
+++ mautrix/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.4.0.dev67"
+__version__ = "0.4.0.dev68"
 __author__ = "Tulir Asokan <tulir@maunium.net>"
 __all__ = ["api", "appservice", "bridge", "client", "errors", "util", "types"]
