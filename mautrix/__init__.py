d61e22d0c336d57983be59746040d841137ca28e
Tulir Asokan <tulir@maunium.net>
2020-07-12 14:50:33 +0300

    Bump version to 0.6.0rc1

--- mautrix/__init__.py
+++ mautrix/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.6.0.beta7"
+__version__ = "0.6.0rc1"
 __author__ = "Tulir Asokan <tulir@maunium.net>"
 __all__ = ["api", "appservice", "bridge", "client", "crypto", "errors", "util", "types"]
