c4949e7c4aec2616ea1879cfe068fc81072dc21c
Tulir Asokan <tulir@maunium.net>
2021-11-20 16:51:33 +0200

    Bump version to 0.12.2

--- mautrix/__init__.py
+++ mautrix/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.12.1"
+__version__ = "0.12.2"
 __author__ = "Tulir Asokan <tulir@maunium.net>"
 __all__ = ["api", "appservice", "bridge", "client", "crypto", "errors", "util", "types"]
