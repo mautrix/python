9aa8fdf053e940f8c7cb7d9e9d4c210022bd1338
Tulir Asokan <tulir@maunium.net>
2021-11-23 00:48:10 +0200

    Include aiohttp in __module__ fixer

--- mautrix/fixmodule.py
+++ mautrix/fixmodule.py
@@ -1,5 +1,11 @@
+# This is a script that fixes the __module__ tags in mautrix-python and some libraries.
+# It's used to help Sphinx/autodoc figure out where the things are canonically imported from
+# (by default, it shows the exact module they're defined in rather than the top-level import path).
+
 from types import FunctionType, ModuleType
 from typing import NewType
+
+import aiohttp
 
 from . import types, bridge, client, crypto, errors, appservice
 from .crypto import attachments
@@ -7,7 +13,7 @@ from .util import async_db, config, db, formatter, logging
 
 
 def _fix(obj: ModuleType) -> None:
-    for item_name in dir(obj):
+    for item_name in getattr(obj, "__all__", None) or dir(obj):
         item = getattr(obj, item_name)
         if isinstance(item, (type, FunctionType, NewType)):
             # Ignore backwards-compatibility imports like the BridgeState import in mautrix.bridge
@@ -23,7 +29,7 @@ def _fix(obj: ModuleType) -> None:
 
 
 _things_to_fix = [types, bridge, client, crypto, attachments, errors, appservice, async_db,
-                  config, db, formatter, logging]
+                  config, db, formatter, logging, aiohttp]
 
 for mod in _things_to_fix:
     _fix(mod)
