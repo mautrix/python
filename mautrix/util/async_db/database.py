36497ac56387e0a25a32c77e750faeac4578b487
Tulir Asokan <tulir@maunium.net>
2021-11-23 12:17:09 +0200

    Remove extra import

--- mautrix/util/async_db/database.py
+++ mautrix/util/async_db/database.py
@@ -3,7 +3,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from typing import Any, List, Awaitable, Type, Dict, Union, Optional, Protocol
+from typing import Any, List, Awaitable, Type, Dict, Union, Optional
 from abc import ABC, abstractmethod
 from urllib.parse import urlparse
 import logging
