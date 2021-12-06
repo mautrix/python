60133d69ad31a8e42908a129ca6ae68cb68d9e08
Alejandro Herrera <bherrera@ikono.com.co>
2021-12-06 11:06:52 -0500

    Correcting imports

--- mautrix/bridge/portal.py
+++ mautrix/bridge/portal.py
@@ -6,7 +6,6 @@
 from __future__ import annotations
 
 from typing import Any, Optional, Tuple
-from collections import defaultdict
 from abc import ABC, abstractmethod
 from collections import defaultdict
 import asyncio
