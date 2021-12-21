7fc0cf08e64877bcd4e3a921ae785240e8db425a
Tulir Asokan <tulir@maunium.net>
2021-12-21 13:01:30 +0200

    Fix type hint

--- mautrix/bridge/async_getter_lock.py
+++ mautrix/bridge/async_getter_lock.py
@@ -3,11 +3,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 import functools
 
 if TYPE_CHECKING:
-    from typing import Any, Awaitable, Callable, ParamSpec
+    from typing import Awaitable, Callable, ParamSpec
 
     Param = ParamSpec("Param")
     Func = Callable[Param, Awaitable[Any]]
