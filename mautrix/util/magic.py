1d6e770fff0f68716cf86688311d085a861dd69b
Tulir Asokan <tulir@maunium.net>
2021-12-31 11:39:37 +0200

    Add docstring for mimetype method

--- mautrix/util/magic.py
+++ mautrix/util/magic.py
@@ -3,7 +3,8 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from typing import Union
+from __future__ import annotations
+
 import functools
 
 import magic
@@ -16,7 +17,21 @@ except AttributeError:
     _from_filename = lambda file: magic.detect_from_filename(file).mime_type
 
 
-def mimetype(data: Union[bytes, str]) -> str:
+def mimetype(data: bytes | str) -> str:
+    """
+    Uses magic to determine the mimetype of a file on disk or in memory.
+
+    Supports both libmagic's Python bindings and the python-magic package.
+
+    Args:
+        data: The file data, either as in-memory bytes or a path to the file as a string.
+
+    Returns:
+        The mime type as a string.
+    """
     if isinstance(data, str):
         return _from_filename(data)
     return _from_buffer(data)
+
+
+__all__ = ["mimetype"]
