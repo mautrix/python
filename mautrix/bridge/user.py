7d29e3df9bcc303935595c972a6f3cc1cf0d8b96
Scott Weber <scott@beeper.com>
2022-01-17 12:24:54 -0500

    Run isort

--- mautrix/bridge/user.py
+++ mautrix/bridge/user.py
@@ -5,7 +5,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from __future__ import annotations
 
-from typing import Any, NamedTuple, Deque
+from typing import Any, Deque, NamedTuple
 from abc import ABC, abstractmethod
 from collections import defaultdict, deque
 import asyncio
