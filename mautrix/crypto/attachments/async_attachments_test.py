79f8969683729482416a6fb4d91a35a67f273484
Tulir Asokan <tulir@maunium.net>
2022-07-05 19:24:32 +0300

    Fix another new test on Python 3.8

--- mautrix/crypto/attachments/async_attachments_test.py
+++ mautrix/crypto/attachments/async_attachments_test.py
@@ -5,6 +5,8 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from __future__ import annotations
+
 from mautrix.types import EncryptedFile
 
 from .async_attachments import async_encrypt_attachment, async_inplace_encrypt_attachment
