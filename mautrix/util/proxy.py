03f54e2be035c5ec711ac7a72ee7130cc0b9fa68
Nick Mills-Barrett <nick@beeper.com>
2023-03-11 18:28:38 +0400

    Fix wait second handling in `proxy_with_retry` (#137)
    * Fix wait second handling in `proxy_with_retry`
    
    Adds new argument `max_wait_seconds` which actually was the broken
    behaviour of the previous `min_wait_seconds` which is now correctly
    handled.
    
    * Add `multiply_wait_seconds` argument

--- mautrix/util/proxy.py
+++ mautrix/util/proxy.py
@@ -90,7 +90,9 @@ async def proxy_with_retry(
     proxy_handler: ProxyHandler,
     on_proxy_change: Callable[[], Awaitable[None]],
     max_retries: int = 10,
-    min_wait_seconds: int = 60,
+    min_wait_seconds: int = 0,
+    max_wait_seconds: int = 60,
+    multiply_wait_seconds: int = 10,
     retryable_exceptions: tuple[Exception] = RETRYABLE_PROXY_EXCEPTIONS,
 ) -> T:
     errors = 0
@@ -102,7 +104,9 @@ async def proxy_with_retry(
             errors += 1
             if errors > max_retries:
                 raise
-            wait = min(errors * 10, min_wait_seconds)
+            wait = errors * multiply_wait_seconds
+            wait = max(wait, min_wait_seconds)
+            wait = min(wait, max_wait_seconds)
             logger.warning(
                 "%s while trying to %s, retrying in %d seconds",
                 e.__class__.__name__,
