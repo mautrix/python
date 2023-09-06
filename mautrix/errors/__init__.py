e9f8bf8189f04755a586bf929fb00126af3296f7
Tulir Asokan <tulir@maunium.net>
2023-09-06 21:24:56 +0300

    Add missing item in errors.__all__

--- mautrix/errors/__init__.py
+++ mautrix/errors/__init__.py
@@ -73,6 +73,7 @@ __all__ = [
     "DeviceValidationError",
     "DuplicateMessageIndex",
     "EncryptionError",
+    "GroupSessionWithheldError",
     "MatchingSessionDecryptionError",
     "MismatchingRoomError",
     "SessionNotFound",
