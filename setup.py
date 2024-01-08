f7b018c501e132d69ec2b3bbe3192efc0f56688f
Tulir Asokan <tulir@maunium.net>
2024-01-08 14:54:03 +0200

    Add ruamel.yaml for tests again

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 from mautrix import __version__
 
 encryption_dependencies = ["python-olm", "unpaddedbase64", "pycryptodome"]
-test_dependencies = ["aiosqlite", "asyncpg", *encryption_dependencies]
+test_dependencies = ["aiosqlite", "asyncpg", "ruamel.yaml", *encryption_dependencies]
 
 setuptools.setup(
     name="mautrix",
